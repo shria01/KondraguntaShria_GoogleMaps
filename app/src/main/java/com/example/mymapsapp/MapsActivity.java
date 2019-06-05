ce1d301bd54689dee3f83bac19af96a34e01ead6
shria01 <47193621+shria01@users.noreply.github.com>
2019-06-05 13:45:27 -0700

    :+1:

---
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -0,0 +1,73 @@
+package com.example.mymapsapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int MY_REQUEST_INT = 177;
+    private GoogleMap mMap;
+    private boolean isGPSEnabled = false;
+    private boolean isNetworkEnabled = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Florida = new LatLng(27.3364, -82.5307);
+        mMap.addMarker(new MarkerOptions().position(Florida).title("Marker in Florida"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Florida));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, MY_REQUEST_INT);
+            }
+            return;
+        }else{
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    public void changeView(View view){
+        if(mMap.getMapType()==1){
+            mMap.setMapType(2);
+        }
+        else if(mMap.getMapType()==2){
+            mMap.setMapType(1);
+        }
+    }
+    public void
+}
